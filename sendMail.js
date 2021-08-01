d8b06b1e4bdd28dabc56a62c5d1e3f771ff878cc
Minh Tài <tranminhtai365@gmail.com>
2021-08-01 17:07:08 +0700

    change location tiktok ads

--- sendMail.js
+++ sendMail.js
@@ -18,22 +18,6 @@ function sendEmail() {
     );
 }
 
-// suppost online
-$(document).ready(function () {
-    $('.support-content').hide();
-    $('a.btn-support').click(function (e) {
-        e.stopPropagation();
-        $('.support-content').slideToggle();
-    });
-    $('.support-content').click(function (e) {
-        e.stopPropagation();
-    });
-    $(document).click(function () {
-        $('.support-content').slideUp();
-    });
-});
-
-
 // Tiktok ads
 // !function (w, d, t) {
 //     w.TiktokAnalyticsObject = t; var ttq = w[t] = w[t] || []; ttq.methods = ["page", "track", "identify", "instances", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie"], ttq.setAndDefer = function (t, e) { t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments, 0))) } }; for (var i = 0; i < ttq.methods.length; i++)ttq.setAndDefer(ttq, ttq.methods[i]); ttq.instance = function (t) { for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++)ttq.setAndDefer(e, ttq.methods[n]); return e }, ttq.load = function (e, n) { var i = "https://analytics.tiktok.com/i18n/pixel/events.js"; ttq._i = ttq._i || {}, ttq._i[e] = [], ttq._i[e]._u = i, ttq._t = ttq._t || {}, ttq._t[e] = +new Date, ttq._o = ttq._o || {}, ttq._o[e] = n || {}; var o = document.createElement("script"); o.type = "text/javascript", o.async = !0, o.src = i + "?sdkid=" + e + "&lib=" + t; var a = document.getElementsByTagName("script")[0]; a.parentNode.insertBefore(o, a) };
@@ -50,3 +34,22 @@ $(document).ready(function () {
     ttq.load('C436UBMI9NESIEHLJKAG');
     ttq.page();
 }(window, document, 'ttq');
+
+
+// suppost online
+$(document).ready(function () {
+    $('.support-content').hide();
+    $('a.btn-support').click(function (e) {
+        e.stopPropagation();
+        $('.support-content').slideToggle();
+    });
+    $('.support-content').click(function (e) {
+        e.stopPropagation();
+    });
+    $(document).click(function () {
+        $('.support-content').slideUp();
+    });
+});
+
+
+
